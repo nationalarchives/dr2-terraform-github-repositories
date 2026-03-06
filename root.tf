4eeaa46453986e17079ff34623e0689eb48569aa
MancunianSam <sam.palmer@nationalarchives.gov.uk>
2026-03-06 11:05:32 +0000

    Add aws-clients repo

--- root.tf
+++ root.tf
@@ -90,3 +90,16 @@ module "dr2-farm-survey" {
     WORKFLOW_TOKEN = data.aws_ssm_parameter.github_workflow_token.value
   }
 }
+
+module "github_aws_clients_repository" {
+  source          = "git::https://github.com/nationalarchives/da-terraform-modules//github_repository_secrets"
+  repository_name = "nationalarchives/da-aws-clients"
+  secrets = {
+    WORKFLOW_TOKEN    = data.aws_ssm_parameter.github_workflow_token.value
+    SLACK_WEBHOOK     = data.aws_ssm_parameter.github_slack_webhook.value
+    SONATYPE_USERNAME = data.aws_ssm_parameter.github_sonatype_username.value
+    SONATYPE_PASSWORD = data.aws_ssm_parameter.github_sonatype_password.value
+    GPG_PRIVATE_KEY   = data.aws_ssm_parameter.github_gpg_key.value
+    GPG_PASSPHRASE    = data.aws_ssm_parameter.github_gpg_passphrase.value
+  }
+}
