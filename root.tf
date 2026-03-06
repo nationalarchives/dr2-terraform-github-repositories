443d16b5d6b383ccf53318ef1ae15625f9c31224
MancunianSam <sam.palmer@nationalarchives.gov.uk>
2026-03-06 12:47:37 +0000

    Add management account

--- root.tf
+++ root.tf
@@ -7,12 +7,13 @@ module "github_preservica_client_repository" {
   source          = "git::https://github.com/nationalarchives/da-terraform-modules//github_repository_secrets"
   repository_name = "nationalarchives/dr2-preservica-client"
   secrets = {
-    WORKFLOW_TOKEN    = data.aws_ssm_parameter.github_workflow_token.value
-    SLACK_WEBHOOK     = data.aws_ssm_parameter.github_slack_webhook.value
-    SONATYPE_USERNAME = data.aws_ssm_parameter.github_sonatype_username.value
-    SONATYPE_PASSWORD = data.aws_ssm_parameter.github_sonatype_password.value
-    GPG_PRIVATE_KEY   = data.aws_ssm_parameter.github_gpg_key.value
-    GPG_PASSPHRASE    = data.aws_ssm_parameter.github_gpg_passphrase.value
+    MANAGEMENT_ACCOUNT = data.aws_caller_identity.current.account_id
+    WORKFLOW_TOKEN     = data.aws_ssm_parameter.github_workflow_token.value
+    SLACK_WEBHOOK      = data.aws_ssm_parameter.github_slack_webhook.value
+    SONATYPE_USERNAME  = data.aws_ssm_parameter.github_sonatype_username.value
+    SONATYPE_PASSWORD  = data.aws_ssm_parameter.github_sonatype_password.value
+    GPG_PRIVATE_KEY    = data.aws_ssm_parameter.github_gpg_key.value
+    GPG_PASSPHRASE     = data.aws_ssm_parameter.github_gpg_passphrase.value
   }
 }
 
